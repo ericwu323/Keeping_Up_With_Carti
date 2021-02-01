33c3e5b1bddb3f99260e86198f3d5e3c0493f6f1
Eric323 <eric323@ucsb.edu>
2021-01-31 22:49:47 -0800

    Alarm system setup, no layout yet

---
+++ app/src/main/java/com/example/keepingupwithcarti/CartiAlarm.java
@@ -0,0 +1,20 @@
+package com.example.keepingupwithcarti;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.example.keepingupwithcarti.Util.Database;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class CartiAlarm extends BroadcastReceiver{
+    public void onReceive(Context context, Intent intent){
+        //DO TASK
+        //insertTask(task);
+        //IDK HOW THIS WORKS
+    }
+}
--- app/src/main/java/com/example/keepingupwithcarti/MainActivity.java
+++ app/src/main/java/com/example/keepingupwithcarti/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.keepingupwithcarti;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +12,10 @@ import com.example.keepingupwithcarti.Model.ToDoModel;
 import com.example.keepingupwithcarti.Util.Database;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +30,7 @@ import android.view.MenuItem;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Calendar;
 import java.util.Objects;
 import java.util.Random;
 
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
     private List<ToDoModel> taskList;
     private FloatingActionButton fab;
     private ListAdapter tasksAdapter;
+    TimePicker timePicker;
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -70,7 +78,24 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
                 mp.start();
             }
         });
+        //alarm
+        //need new button for alarm thing
+        /*findViewById(R.id.REPLACEBUTTONID).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                if (android.os.Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getHour(), timePicker.getMinute(), 0);
+                } else {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getCurrentHour(), timePicker.getCurrentMinute(), 0);
+                }
 
+
+                setAlarm(calendar.getTimeInMillis());
+            }
+        });*/
     }
 
 
@@ -101,5 +126,19 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
         Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
         tasksAdapter.notifyDataSetChanged();
+    }
+    private void setAlarm(long time) {
+        //getting the alarm manager
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        //creating a new intent specifying the broadcast receiver
+        Intent i = new Intent(this, CartiAlarm.class);
+
+        //creating a pending intent using the intent
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+
+        //setting the repeating alarm that will be fired every day
+        am.setRepeating(AlarmManager.RTC, time, AlarmManager.INTERVAL_DAY, pi);
+        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show();
     }
 }
